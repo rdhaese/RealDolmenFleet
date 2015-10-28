b2cf370bbf84e499250dcba11c0841e3e05c78d8
Robin D'Haese <r.dhaese92@gmail.com>
2015-10-28 14:22:07 +0100

    need to commit before pull

--- webapp/src/main/java/com/realdolmen/fleet/config/SecurityConfig.java
+++ webapp/src/main/java/com/realdolmen/fleet/config/SecurityConfig.java
@@ -2,16 +2,11 @@ package com.realdolmen.fleet.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.expression.spel.support.StandardEvaluationContext;
-import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
-import org.springframework.security.authentication.encoding.PasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.sql.DataSource;
