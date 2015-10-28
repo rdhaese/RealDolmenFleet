7c47508e065ffc6ad16f56aef2cda5ebbf876cd8
Robin D'Haese <r.dhaese92@gmail.com>
2015-10-28 08:56:45 +0100

    logging has been setup in application.properties

--- webapp/src/main/java/com/realdolmen/fleet/ApplicationConfiguration.java
+++ webapp/src/main/java/com/realdolmen/fleet/ApplicationConfiguration.java
@@ -1,6 +1,7 @@
 package com.realdolmen.fleet;
 
 import config.Config;
+import org.apache.log4j.Logger;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -12,7 +13,10 @@ import org.springframework.context.ConfigurableApplicationContext;
  */
 @SpringBootApplication
 public class ApplicationConfiguration {
+    private static final Logger LOGGER = Logger.getLogger(ApplicationConfiguration.class);
     public static void main (String[] args){
+        LOGGER.info("APPLICATION STARTUP");
         ConfigurableApplicationContext context = new SpringApplicationBuilder(ApplicationConfiguration.class).profiles("production").build().run(args);
+        LOGGER.info("APPLICATION STARTED");
     }
 }
