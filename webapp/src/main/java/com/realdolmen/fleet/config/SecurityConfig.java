36d43fe0126f42a1642946562d91fd590b75d386
Robin D'Haese <r.dhaese92@gmail.com>
2015-10-28 10:19:19 +0100

    added fleet.logs to .gitignore

--- webapp/src/main/java/com/realdolmen/fleet/config/SecurityConfig.java
+++ webapp/src/main/java/com/realdolmen/fleet/config/SecurityConfig.java
@@ -1,10 +1,15 @@
 package com.realdolmen.fleet.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+
 /**
  * Created on 28/10/2015.
  *
  * @author Robin D'Haese
  */
+@Configuration
+@EnableWebMvcSecurity
 public class SecurityConfig {
 
 }
