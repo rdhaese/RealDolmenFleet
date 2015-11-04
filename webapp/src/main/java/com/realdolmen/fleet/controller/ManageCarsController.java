394a5a56ed3b19662389b49ae59efc7ccc82a50d
Robin D'Haese <r.dhaese92@gmail.com>
2015-11-04 08:37:44 +0100

    commit before pull, working on car detail for fleet

--- webapp/src/main/java/com/realdolmen/fleet/controller/ManageCarsController.java
+++ webapp/src/main/java/com/realdolmen/fleet/controller/ManageCarsController.java
@@ -5,6 +5,7 @@ import com.realdolmen.fleet.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -26,4 +27,12 @@ public class ManageCarsController {
         model.addAttribute("ourCarsInFreePool", carService.findAllFromFreePool());
         return "fleet/cars";
     }
+
+    @RequestMapping(value="/fleet/cars/detail/{carUsageId}", method = RequestMethod.GET)
+    public String carDetail(@PathVariable("carUsageId") Long carUsageId, Model model){
+        model.addAttribute("carDetails", carService.findCarUsageById(carUsageId));
+        return "fleet/car-detail";
+    }
+
+
 }
